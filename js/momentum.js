236ce678d48e79053df70231a1e88aedd2c35c96
Scott Kellum <scott@typetura.com>
2025-02-12 01:07:19 -0800

    Optimize

--- js/momentum.js
+++ js/momentum.js
@@ -2,139 +2,131 @@
 
     var momentumRoot;
     var momentumEls;
+    var scrollX, scrollY, clientX, clientY, pointerDown, touches, orientAlpha, orientBeta, orientGamma, compassHeading;
 
-    // Page and element location and dimentions
+    // Page and element location and dimensions
     function momentumInit() {
-        momentumRoot.setProperty('--viewportwidth',window.innerWidth);
-        momentumRoot.setProperty('--viewportheight',window.innerHeight);
-        for (i = 0; i < momentumEls.length; i++) {
+        momentumRoot.setProperty('--viewportwidth', window.innerWidth);
+        momentumRoot.setProperty('--viewportheight', window.innerHeight);
+        for (var i = 0; i < momentumEls.length; i++) {
             var e = momentumEls[i];
-            e.style.setProperty('--width',e.offsetWidth);
-            e.style.setProperty('--height',e.offsetHeight);
-            e.style.setProperty('--top',e.offsetTop);
-            e.style.setProperty('--left',e.offsetLeft);
-            e.style.setProperty('--random',Math.random());
+            e.style.setProperty('--width', e.offsetWidth);
+            e.style.setProperty('--height', e.offsetHeight);
+            e.style.setProperty('--top', e.offsetTop);
+            e.style.setProperty('--left', e.offsetLeft);
+            e.style.setProperty('--random', Math.random());
         }
-    };
+    }
+
+    function updateProperties() {
+        momentumRoot.setProperty('--scrollx', scrollX);
+        momentumRoot.setProperty('--scrolly', scrollY);
+        momentumRoot.setProperty('--clientx', clientX);
+        momentumRoot.setProperty('--clienty', clientY);
+        momentumRoot.setProperty('--pointerdown', pointerDown);
+        momentumRoot.setProperty('--touches', touches);
+        momentumRoot.setProperty('--orientalpha', orientAlpha);
+        momentumRoot.setProperty('--orientbeta', orientBeta);
+        momentumRoot.setProperty('--orientgamma', orientGamma);
+        momentumRoot.setProperty('--compassheading', compassHeading);
+    }
+
+    function requestUpdate() {
+        window.requestAnimationFrame(updateProperties);
+    }
 
     // When the DOM is loaded
-    document.addEventListener( 'DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
         momentumRoot = document.body.style;
         momentumEls = document.querySelectorAll('.momentumcss');
-        momentumRoot.setProperty('--loaded',0);
+        momentumRoot.setProperty('--loaded', 0);
 
         // Initialize variables
-        momentumRoot.setProperty('--scrollx',window.scrollX);
-        momentumRoot.setProperty('--scrolly',window.scrollY);
-        momentumRoot.setProperty('--clientx','0');
-        momentumRoot.setProperty('--clienty','0');
-        momentumRoot.setProperty('--pointerdown',0);
-        momentumRoot.setProperty('--touches',0);
-        momentumRoot.setProperty('--orientalpha','0');
-        momentumRoot.setProperty('--orientbeta','0');
-        momentumRoot.setProperty('--orientgamma','0');
-        momentumRoot.setProperty('--compassheading','0');
-        momentumRoot.setProperty('--random',Math.random());
-        momentumRoot.setProperty('--noise',Math.random());
+        scrollX = window.scrollX;
+        scrollY = window.scrollY;
+        clientX = 0;
+        clientY = 0;
+        pointerDown = 0;
+        touches = 0;
+        orientAlpha = 0;
+        orientBeta = 0;
+        orientGamma = 0;
+        compassHeading = 0;
 
+        momentumRoot.setProperty('--random', Math.random());
+        momentumRoot.setProperty('--noise', Math.random());
 
         // Scroll events
         window.addEventListener('scroll', function() {
-            window.requestAnimationFrame(momentumScroll);
+            scrollX = window.scrollX;
+            scrollY = window.scrollY;
+            requestUpdate();
         }, false);
-        
-        function momentumScroll() {
-            momentumRoot.setProperty('--scrollx',window.scrollX);
-            momentumRoot.setProperty('--scrolly',window.scrollY);
-        }
 
         // Pointer events
         window.addEventListener('pointermove', function(e) {
-            window.requestAnimationFrame(function() {
-                momentumPointer(e);
-            });
+            clientX = e.clientX;
+            clientY = e.clientY;
+            requestUpdate();
         }, false);
-        function momentumPointer(e) {
-            momentumRoot.setProperty('--clientx',e.clientX);
-            momentumRoot.setProperty('--clienty',e.clientY);
-        }
+
         window.addEventListener('pointerdown', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumPointerDown(e);
-            });
+            pointerDown = 1;
+            requestUpdate();
         }, false);
-        
+
         window.addEventListener('pointerup', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumPointerUp(e);
-            });
+            pointerDown = 0;
+            requestUpdate();
         }, false);
-        function momentumPointerDown(e) {
-            momentumRoot.setProperty('--pointerdown',1);
-        }
-        function momentumPointerUp(e) {
-            momentumRoot.setProperty('--pointerdown',0);
-        }
+
         // Override pointer events if touch events
         window.addEventListener('touchmove', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumTouch(e);
-            });
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
+            requestUpdate();
         }, false);
-        function momentumTouch(e) {
-            momentumRoot.setProperty('--clientx',e.touches[0].clientX);
-            momentumRoot.setProperty('--clienty',e.touches[0].clientY);
-        }
+
         window.addEventListener('touchstart', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumTouchStart(e);
-            });
+            pointerDown = 0;
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
+            touches = e.targetTouches.length;
+            requestUpdate();
         }, false);
-        
+
         window.addEventListener('touchend', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumTouchEnd(e);
-            });
+            touches = e.targetTouches.length;
+            requestUpdate();
         }, false);
-        function momentumTouchStart(e) {
-            momentumRoot.setProperty('--pointerdown',0);
-            momentumRoot.setProperty('--clientx',e.touches[0].clientX);
-            momentumRoot.setProperty('--clienty',e.touches[0].clientY);
-            momentumRoot.setProperty('--touches',e.targetTouches.length);
-        }
-        function momentumTouchEnd(e) {
-            momentumRoot.setProperty('--touches',e.targetTouches.length);
-        }
 
         // Device orientation
         window.addEventListener('deviceorientation', function(e) {
-            window.requestAnimationFrame(function() {
-            momentumOrientation(e);
-            });
+            orientAlpha = e.alpha;
+            orientBeta = e.beta;
+            orientGamma = e.gamma;
+            compassHeading = e.webkitCompassHeading;
+            requestUpdate();
         });
-        function momentumOrientation(e) {
-            momentumRoot.setProperty('--orientalpha',e.alpha);
-            momentumRoot.setProperty('--orientbeta',e.beta);
-            momentumRoot.setProperty('--orientgamma',e.gamma);
-            momentumRoot.setProperty('--compassheading',e.webkitCompassHeading);
-        }
+
         momentumInit();
-    }, false );
+    }, false);
 
     // When the page has fully loaded
-    window.onload = function(){
+    window.onload = function() {
         momentumInit();
-        momentumRoot.setProperty('--loaded',1);
+        momentumRoot.setProperty('--loaded', 1);
 
         // Noise
         var i = window.getComputedStyle(document.body).getPropertyValue('--noise-interval');
         if (i != '0') {
-            window.setInterval(function(){
-                momentumRoot.setProperty('--noise',Math.random());
+            window.setInterval(function() {
+                momentumRoot.setProperty('--noise', Math.random());
             }, i || 100);
         } else {
-            momentumRoot.setProperty('--noise',Math.random());
+            momentumRoot.setProperty('--noise', Math.random());
         }
     };
-    window.addEventListener('resize',momentumInit);
+
+    window.addEventListener('resize', momentumInit);
 })();
